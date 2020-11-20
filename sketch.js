295b6a56dd2bc51a268be5ec40efcea024c6eda6
Strider1Trigger <70731975+Strider1Trigger@users.noreply.github.com>
2020-11-19 21:23:00 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var wall1,wall2,wall3
+var ground,paper
+
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	wall1=new Dustbin(width/2+130,610,20,100)
+	wall2=new Dustbin(width/2+220,650,200,20)
+	wall3=new Dustbin(width/2+330,610,20,100)
+	ground=new Ground(400,670,800,10)
+	paper=new Paper(100,100,20)
+
+	Engine.run(engine);
+  
+}
+
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  wall1.display()
+  wall2.display()
+  wall3.display()
+  ground.display()
+  paper.display()
+ 
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+
+   Body.applyForce(paper.body,paper.body.position,{x:85,y:-85})
+
+  }
+}
+
+
+
